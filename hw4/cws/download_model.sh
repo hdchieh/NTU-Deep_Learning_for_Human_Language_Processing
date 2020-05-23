0a001b47e49c58c73bb4a5c677e825a66bae81c1
En <r08922a02@ntu.edu.tw>
2020-05-24 01:29:39 +0800

    edit cws folder

--- hw4/cws/download_model.sh
+++ hw4/cws/download_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Download dataset from Google Drive
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uwp-dL-LYF1nebP0J8wZQjJ0T5B88swV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1uwp-dL-LYF1nebP0J8wZQjJ0T5B88swV" -O OUT_DIR/my_cws_bert.pt && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uwp-dL-LYF1nebP0J8wZQjJ0T5B88swV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1uwp-dL-LYF1nebP0J8wZQjJ0T5B88swV" -O ./my_cws_bert.pt && rm -rf /tmp/cookies.txt
